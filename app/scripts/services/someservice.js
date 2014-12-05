6760d07a32f1bb403cd4403f7ebe872faf2d4175
Ladex <ladipoilori@yahoo.com>
2014-12-05 19:25:29 +0000

    added renamed file

---
+++ app/scripts/services/someservice.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name sampleSublimeFolderApp.someServie
+ * @description
+ * # someServie
+ * Factory in the sampleSublimeFolderApp.
+ */
+angular.module('sampleSublimeFolderApp')
+  .factory('someService', function () {
+    // Service logic
+    // ...
+
+    var meaningOfLife = 42;
+
+    // Public API here
+    return {
+      someMethod: function () {
+        return meaningOfLife;
+      }
+    };
+  });
